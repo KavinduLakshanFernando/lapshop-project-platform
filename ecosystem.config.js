5c27d5d14b71a1927bbec372b028af763bd11de4
KavinduLakshanFernando <kavindu2003fernando@gmail.com>
2026-03-30 23:03:43 +0530

    log_files_new

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,28 @@
 module.exports = {
-  apps : [
+  apps: [
     {
       name: "config-server",
       script: "java",
       args: "-jar ./config-server/target/config-server-1.0.0.jar",
-      cwd: "C:/IJSE/GCP/Cloud/LapShop/platform",
+      cwd: "/home/kavindu009lakshan/lapshop-project-platform",
       interpreter: "none",
-      log_file: ".logs/config-server.log",
+      log_file: "/home/kavindu009lakshan/lapshop-project-platform/.logs/config-server.log",
     },
     {
       name: "eureka-server",
       script: "java",
       args: "-jar ./eureka-server/target/eureka-server-1.0.0.jar",
-      cwd: "C:/IJSE/GCP/Cloud/LapShop/platform",
+      cwd: "/home/kavindu009lakshan/lapshop-project-platform",
       interpreter: "none",
-      log_file: ".logs/eureka-server.log",
+      log_file: "/home/kavindu009lakshan/lapshop-project-platform/.logs/eureka-server.log",
     },
     {
       name: "api-gateway",
       script: "java",
       args: "-jar ./api-gateway/target/api-gateway-1.0.0.jar",
-      cwd: "C:/IJSE/GCP/Cloud/LapShop/platform",
+      cwd: "/home/kavindu009lakshan/lapshop-project-platform",
       interpreter: "none",
-        log_file: ".logs/api-gateway.log",
+      log_file: "/home/kavindu009lakshan/lapshop-project-platform/.logs/api-gateway.log",
     }
   ]
 }
